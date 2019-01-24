ab06ff4a11ab0037857e5344377f851e8a0ca164
lgsgithub13 <m18658756225@163.com>
2019-01-24 12:51:12 +0800

    添加菜单、按钮

--- 自动安装器/app/build/gen/com/autoinstaller/lgsapp/R.java
+++ 自动安装器/app/build/gen/com/autoinstaller/lgsapp/R.java
@@ -13,8 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int item1=0x7f070003;
+        public static final int item2=0x7f070004;
+        public static final int mainButton1=0x7f070000;
+        public static final int mainButton2=0x7f070001;
+        public static final int setitemListView1=0x7f070002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int set_item=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- 自动安装器/app/src/main/java/com/autoinstaller/lgsapp/MainActivity.java
+++ 自动安装器/app/src/main/java/com/autoinstaller/lgsapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.autoinstaller.lgsapp;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.provider.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener,DialogInterface.OnClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -11,4 +15,58 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater mi=getMenuInflater();
+		mi.inflate(R.menu.main_menu,menu);
+		// TODO: Implement this method
+		return super.onCreateOptionsMenu(menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case R.id.item1:
+				Toast.makeText(MainActivity.this,"QQ:3377017440",Toast.LENGTH_LONG).show();
+				break;
+			case R.id.item2:
+				LayoutInflater li=LayoutInflater.from(this);
+				View setItemView=li.inflate(R.layout.set_item,null);
+				ListView lv=(ListView)setItemView.findViewById(R.id.setitemListView1);
+				ArrayAdapter<String> adapter=new ArrayAdapter<String>
+				(
+					MainActivity.this
+					,android.R.layout.simple_list_item_1
+					,new String[]
+					{
+						"打开辅助安装服务"
+					}
+				);
+				lv.setAdapter(adapter);
+				lv.setOnItemClickListener(this);
+				new AlertDialog.Builder(this)
+				.setTitle("设置")
+				.setView(setItemView)
+				.setCancelable(false)
+				.setNegativeButton("关闭",this)
+				.show();
+				break;
+		}
+		// TODO: Implement this method
+		return super.onOptionsItemSelected(item);
+	}
+	@Override
+	public void onItemClick(AdapterView<?> p1, View p2, int p3, long p4)
+	{
+		Intent i=new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+		startActivity(i);
+		// TODO: Implement this method
+	}
+	@Override
+	public void onClick(DialogInterface p1, int p2)
+	{
+		// TODO: Implement this method
+	}
 }
